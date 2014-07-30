74b5ea64f93858f72874fce460a13f40b7dd4db4
Mathias Buus <mathiasbuus@gmail.com>
2014-07-30 16:58:07 +0200

    update style

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var valid = /^[a-zA-Z_][a-zA-Z_0-9]*$/;
+var valid = /^[a-zA-Z_][a-zA-Z_0-9]*$/
 
 var gen = function(obj, prop) {
-	if (valid.test(prop)) return obj+'.'+prop;
-	return obj+'['+JSON.stringify(prop)+']'
-};
+  if (valid.test(prop)) return obj+'.'+prop
+  return obj+'['+JSON.stringify(prop)+']'
+}
 
-gen.valid = valid;
-module.exports = gen;
+gen.valid = valid
+module.exports = gen
